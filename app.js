d48b6a5ebda061b934127d5b3cb4b595beb2b4f0
yaswanth-git <yaswanth.offl@gmail.com>
2024-12-24 18:52:31 +0530

    ntlify configs added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/index");
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use("/", require("./api/crud"));
+
+module.exports = app;
