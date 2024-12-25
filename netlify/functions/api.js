f8ab30a6a3eae16c7a21070dda7fd0f8b6cd8289
yaswanth-git <yaswanth.offl@gmail.com>
2024-12-25 16:59:31 +0530

    dist added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http');
 const app = require('../../app.js');
 
+app.use("/.netlify/functions/api", require("./api/crud"));
 module.exports.handler = serverless(app);
