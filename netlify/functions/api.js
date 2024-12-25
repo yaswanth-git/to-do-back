ee84431e6605a7dfb9b50526ad6548ab941d0866
yaswanth-git <yaswanth.offl@gmail.com>
2024-12-25 17:09:46 +0530

    directory fixed

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 const serverless = require('serverless-http');
 const app = require('../../app.js');
 
-app.use("/.netlify/functions/api", require("./api/crud"));
+app.use("/.netlify/functions/api", require("../../api/crud"));
 module.exports.handler = serverless(app);
